f84cab16492f61aa8ccac6f07d486b4afbee89a5
BeatrizAcostaa <beatrizzaacosta29@gmail.com>
2024-12-30 18:34:50 -0800

    New Changes!

--- chefsTable/chefsTableApp/views.py
+++ chefsTable/chefsTableApp/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .forms import BookingForm
 
 def home(request):
-    return HttpResponse("<h1>Welcome to Little Lemon!</h1>")
-# Create your views here.S
+    return HttpResponse("Welcome to Little Lemon!")
+
+def booking_view(request):
+    if request.method == 'POST':
+        form = BookingForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return render(request, 'success.html', {'form': form})
+    else:
+        form = BookingForm()
+    
+    return render(request, 'booking.html', {'form': form})
